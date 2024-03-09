9570f388da4229979ddf9ac2f9298f2e523f22d4
Daniel Fitzgerald <dfitzgerald@mun.ca>
2024-03-09 00:32:27 -0330

    added background image to root route

--- app/page.js
+++ app/page.js
@@ -1,15 +1,18 @@
 import { NavBar } from "./components/navbar";
+import "./layout.css";
 import ContinueButton from "./continuebutton";
 
 export default async function Home() {
   return(
     <main className="h-[100vh]">
       <NavBar />
-      <div className="flex flex-col text-center items-center justify-center h-[calc(100%-60px)] mx-5 overflow-hidden">
-        <div className="text-xl">
-          PinMyLocation is an app allowing you to share your location with others.
+      <div className="backdrop-blur-md h-[calc(100%-60px)] ">
+        <div className="flex flex-col text-center h-full items-center justify-center mx-5 overflow-hidden">
+          <div className="text-xl">
+            PinMyLocation is an app allowing you to share your location with others.
+          </div>
+          <ContinueButton />
         </div>
-        <ContinueButton />
       </div>
     </main>
   );
