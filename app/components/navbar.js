f40bbc7b96e9d257401e58e4c4906cb37bcc76b2
Daniel Fitzgerald <dfitzgerald@mun.ca>
2024-03-11 22:58:10 -0230

    Added project information to home page.

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,23 +1,36 @@
 import Image from "next/image";
 import Link from "next/link";
+import SignUpButton from "./signupbutton";
 
 export function NavBar({}) {
   return(
-    <nav className="bg-[var(--navbar-background)] text-[#FFF67E] text-center font-bold shadow-md">
-      <Link href="/" className="flex flex-row justify-center items-center gap-x-2">
-        <div>
-          <Image
-            className="inline-block"
-            src="/pushpin.png"
-            alt="PinMyLocation PushPin Logo"
-            height={32}
-            width={16}
-          />
+    <nav className="bg-[var(--navbar-background)] text-[#FFF67E] font-bold shadow-md py-2 px-5 md:px-10">
+      <div className="flex items-center justify-between">
+        <div className="w-1/4">
+          <Link href="/" className="flex flex-row items-center gap-x-2">
+            <div className="shrink-0">
+              <Image
+                src="/pushpin.png"
+                alt="PinMyLocation PushPin Logo"
+                height={32}
+                width={16}
+              />
+            </div>
+            <h1 className="text-[22pt] inline-block">
+              PinMyLocation
+            </h1>
+          </Link>
         </div>
-        <h1 className="flex-shrink text-[22pt] p-2">
-          PinMyLocation
-        </h1>
-      </Link>
+        <div className="hidden lg:flex flex-row justify-center align-middle gap-x-5">
+          
+        </div>
+
+        <div className="text-right w-1/4">
+          <div className="lg:inline-block">
+            <SignUpButton />
+          </div>
+        </div>
+      </div>
     </nav>
   );
 }
--- app/continuebutton.js
+++ app/continuebutton.js
@@ -1,17 +1,26 @@
 import { getSession } from '@auth0/nextjs-auth0';
 import { Suspense } from 'react';
 import BigLink from '@/app/components/biglink';
-import BigLinkFallback from './components/biglinkfallback';
+import BigLinkFallback from '@/app/components/biglinkfallback';
 
 async function AwaitButton() {
   const session = await getSession();
   const user = session ? session.user : undefined;
 
+  const { name } = user ? await prisma.user.findUnique({
+    where: {
+      email: user.email,
+    },
+    select: {
+      name: true,
+    }
+  }) : { name: undefined };
+
   return <BigLink
     width="20rem"
     href={user ? "/my-maps" : "/api/auth/login"}
   >
-    {user ? "Continue as " + user.name + "" : "Sign in"}
+    {user ? "Continue as " + name + "" : "Sign in/Create Account"}
   </BigLink>
 }
 
